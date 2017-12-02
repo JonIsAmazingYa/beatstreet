ebd089638ceed97f73defb5db7a7f018eb0a7b06
slars <s.larsendisney@gmail.com>
2017-12-02 17:33:05 +0000

    added location and play uri method

--- src/musicPlayer/SpotifyPlayer.js
+++ src/musicPlayer/SpotifyPlayer.js
@@ -6,11 +6,19 @@ import album from "../assets/duffy.PNG";
 import MapView from "../mapview/Mapview";
 import SpotifyWebApi from 'spotify-web-api-node';
 import * as actionHandler from '../spotifyPlayer/Spotify-Interface.js'
+import {getCurrentTrack} from "../spotifyPlayer/Spotify-Interface";
 
 class SpotifyPlayer extends Component {
     constructor(props) {
         super(props);
-        this.state = {playSwitch:  true, image: play, currentSong: 'No Song', currentArtist: 'No Artist', albumart: '../assets/duffy.PNG'};
+        this.state = {
+            playSwitch:  true,
+            image: play,
+            currentSong: 'No Song',
+            currentArtist: 'No Artist',
+            albumart: '../assets/duffy.PNG',
+            location: 'No Location'
+        };
     }
     componentDidMount(){
         this.getCurrentTrack();
@@ -25,6 +33,22 @@ class SpotifyPlayer extends Component {
             console.log(response);
             this.setState({currentSong: response.body.item.name, currentArtist: response.body.item.artists[0].name, albumart: response.body.item.album.images[0].url});
     })
+    }
+
+    getCurrentLocation(coords){
+        this.setState({location: coords})
+    }
+
+    playSongs(uri, location) {
+        var spotifyApi = new SpotifyWebApi();
+
+        spotifyApi.setAccessToken(localStorage.getItem('accessToken'));
+
+        spotifyApi.play({"uris": uri}).then((response) => {
+            console.log(response);
+            getCurrentTrack();
+            this.getCurrentLocation(location)
+            });
     }
 
     pressSwitch() {
@@ -44,9 +68,8 @@ class SpotifyPlayer extends Component {
 
     render() {
         return (
-            <div className="topLevel">
-                    <h1 className="Brand">BeatStreet</h1>
                     <div className="Background">
+                        <h1 className="Brand">BeatStreet</h1>
                         <MapView/>
                         <div className="Bar">
                             <div className="ButtonHolder">
@@ -54,14 +77,12 @@ class SpotifyPlayer extends Component {
                             </div>
                             <img className="Album" src={this.state.albumart} />
                             <div className="MediaInfo">
-                                <h1 className={"MusicSong"}>Street</h1>
+                                <h2 className={"MusicArtist"}>Street</h2>
                                 <h1 className={"MusicSong"}>{this.state.currentSong}</h1>
                                 <h2 className={"MusicArtist"}>{this.state.currentArtist}</h2>
                             </div>
                         </div>
-                        <img className="SmallButton" src={require("../assets/arrow.svg")} onClick={() => window.scrollTo(0,document.body.scrollHeight)} />
                     </div>
-            </div>
         );
     }
 }
