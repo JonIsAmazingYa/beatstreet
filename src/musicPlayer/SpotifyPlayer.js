55258e0b30a59a1872d1bf2ef8770519f74e3095
JonIsAmazingYa <eurofight@hotmail.co.uk>
2017-12-02 15:43:57 +0000

    Add track info to spotify player

--- src/musicPlayer/SpotifyPlayer.js
+++ src/musicPlayer/SpotifyPlayer.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-
 import './SpotifyPlayer.css';
 import play from "../assets/play.svg";
 import pause from "../assets/pause.svg";
@@ -13,16 +12,16 @@ class SpotifyPlayer extends Component {
         this.state = {playSwitch:  true, image: play};
     }
 
-    pressSwitch(){
+    pressSwitch() {
         if (this.state.playSwitch) {
-            this.setState({playSwitch: false, image: pause});
-
-            actionHandler.pauseSong();
-        }
-        else {
-            this.setState({playSwitch: true, image: play});
+            this.setState({playSwitch: false, image: play});
 
             actionHandler.resumeSong();
+        }
+        else {
+            this.setState({playSwitch: true, image: pause});
+
+            actionHandler.pauseSong();
         }
     }
 
@@ -36,11 +35,11 @@ class SpotifyPlayer extends Component {
                             <div className="ButtonHolder">
                                 <img className="Play" onClick={() => this.pressSwitch()} src={this.state.image} />
                             </div>
-                            <img className="Album" src={require("../assets/duffy.PNG")} />
+                            <img className="Album" src={actionHandler.getCurrentTrack().album.images.0.url} />
                             <div className="MediaInfo">
                                 <h1 className={"MusicSong"}>Street</h1>
-                                <h1 className={"MusicSong"}>Song Title</h1>
-                                <h2 className={"MusicArtist"}>Artist Name</h2>
+                                <h1 className={"MusicSong"}>{actionHandler.getCurrentTrack().name}</h1>
+                                <h2 className={"MusicArtist"}>{actionHandler.getCurrentTrack().artists[0].name}</h2>
                             </div>
                         </div>
                         <img className="SmallButton" src={require("../assets/arrow.svg")} onClick={() => window.scrollTo(0,document.body.scrollHeight)} />
